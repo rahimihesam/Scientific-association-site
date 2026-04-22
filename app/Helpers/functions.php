db74ea0705218ef6c5ba0616012d438a89e3df8d
Ho3ein <mehajho3ein@gmail.com>
2026-04-22 23:02:13 +0330

    feat: Add input type detection helper

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,5 +1,23 @@
 <?php
 
+// Detect input string is mobile number or email address
+function detectedInputType($input)
+{
+  $input = trim($input);
+
+  // Check for iranian mobile number format (09xxxxxxxxx)
+  if (preg_match('/^09[0-9]{9}$/', $input)) {
+    return 'mobile';
+  }
+
+  // Check for valid Gmail address
+  if (filter_var($input, FILTER_VALIDATE_EMAIL) && preg_match('/@gmail\.com$/', $input)) {
+    return 'email';
+  }
+
+  return false;
+}
+
 // HTTP Redirect
 function redirect($url)
 {
