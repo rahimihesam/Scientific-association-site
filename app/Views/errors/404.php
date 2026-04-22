80e514e94daf0a76929cf9a8d7ba125a70dc0425
Ho3ein <mehajho3ein@gmail.com>
2026-04-22 21:57:10 +0330

    add 404 error page

---
+++ app/Views/errors/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>صفحه 404</title>
+</head>
+
+<body>
+
+  <div class="container">
+    <h1>404 - صفحه پیدا نشد</h1>
+    <a href="/">بازگشت به خانه</a>
+  </div>
+
+</body>
+
+</html>
